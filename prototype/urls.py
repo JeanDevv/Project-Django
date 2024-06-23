e45a0aa12001c57fa1a4d8731620230aa09de718
JeanDevv <cjean8164@gmail.com>
2024-06-23 13:53:37 -0300

    Limpeza na raiz do django

--- prototype/urls.py
+++ prototype/urls.py
@@ -13,23 +13,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+""" 
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-def home(request):
-    return HttpResponse('Home')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato)
+    path('', include('recipes.urls'))
 ]
