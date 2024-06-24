d0d137767a5e553de73d25562de715833e9fe29f
JeanDevv <cjean8164@gmail.com>
2024-06-23 21:19:51 -0300

    Apps de contato e sobre foram removidos

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
